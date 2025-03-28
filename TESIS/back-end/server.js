06c9d281dbd6a6b067b976cd8d0c2d2b39d9cc25
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2025-03-28 00:36:16 -0500

    AnyChanges

--- TESIS/back-end/server.js
+++ TESIS/back-end/server.js
@@ -12,13 +12,13 @@ const { Chart } = require("chart.js");
 
 const app = express();
 app.use(cors());
-const corsOptions = {
+/* const corsOptions = {
   origin: "https://frontend-inventario-blond.vercel.app",
   methods: "GET,PUT,POST,DELETE,HEAD,PATCH",
   credentials: true,
   optionsSuccessStatus: 204,
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); */
 app.use(express.json());
 const SECRET_KEY =
   "078e61ddf7838590981558fbe12e7ca5a101095f7d7d84975fb26d4892e0ee83a7a8de0b7e0b62bf14ca9787f516c7810dfbe8930f781275e78d38e0457ac5a5";
